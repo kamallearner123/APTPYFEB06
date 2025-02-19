3ff41303d19df62752153758ebf721cea70fc4c8
kamallearner123 <kamalbec2004@gmail.com>
2025-02-19 09:04:38 +0530

    Dicts

---
+++ Assignments/Adithya/Lists_Strings/Excersizes/Dictionaries.py
@@ -0,0 +1,29 @@
+d1 = {"David": 
+              {"Full Name": "David John", 
+               "Age":       10, 
+               "Subs":      ("Eng", "Maths")},
+      "Ram":
+                {"Full Name": "Ram Kumar", 
+                 "Age":       12, 
+                 "Subs":      ("Eng", "Maths", "Science")},
+        "Saleem":
+                {"Full Name": "Saleem Khan", 
+                 "Age":       13, 
+                 "Subs":      ("Eng", "Science", "Social")},
+        "Ravi":
+                {"Full Name": "Ravi Kumar", 
+                 "Age":       14, 
+                 "Subs":      ("Eng", "Maths", "Social")},
+        "Raj":
+                {"Full Name": "Raj Kumar", 
+                 "Age":       15}
+}
+
+print(d1)
+print(d1["David"])
+print(d1["David"]["Subs"])
+print(d1["David"]["Subs"][0])
+
+# Write a function to list students who have "Science" as one of their subjects.
+
+
